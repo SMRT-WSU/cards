3ff7e9e644b180feabd854631d32370bf0d50888
Luckyme66 <51961563+Luckyme66@users.noreply.github.com>
2019-09-18 19:49:03 +0100

    Update client.py

--- client.py
+++ client.py
@@ -15,7 +15,7 @@ class Listen(Thread):
     def __innit__(self,ip,port,buffer):
         Thread.innit(self)
         self.ip = ip
-        self.port = port
+        self.pot = port
         self.buffer = buffer
         print ('New connection from ', ip)
 
