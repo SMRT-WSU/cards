3fdf7f4b3ea158af97c14313d1c0e726b39ef4e7
ImAFookingScarecrow <51989378+ImAFookingScarecrow@users.noreply.github.com>
2019-09-24 19:31:22 +0100

    So, so many problems...
    here we go:
    - colours of the cards are being changed, or are even just leaving holes. Why? not a fucking clue!
    - any face cards are just replaced with the nine of their suit. Again, not a clue why...
    okay, so maybe not as many issues as I thought, but I am getting very pissed off and need a break from this shit. Fuck pygame!

---
+++ Cards/cards.py
@@ -0,0 +1,51 @@
+import pygame
+import random
+pygame.init()
+
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+green = (0, 112, 0)
+
+suits = ('C', 'D', 'H', 'S')
+values = ('A', 'J', 'K', 'Q')
+deck = []
+hand = []
+
+class card():
+    def __init__(self, value, suit, image):
+        self.value = value
+        self.suit = suit
+        self.image = image
+
+
+for suit in suits:
+    for i in range(2,10):
+        img = pygame.image.load('cards/' + str(i) + suit + '.jpg')
+        screen.blit(img, (0, 0))
+        deck.append(card(str(i), suit, img))
+    for value in values:
+        pygame.image.load('cards/' + value + suit + '.jpg')
+        deck.append(card(value, suit, img))
+
+random.shuffle(deck)
+hand = deck[0:13]
+
+mainLoop = True
+
+infoObject = pygame.display.Info()
+x = infoObject.current_w
+y = infoObject.current_h
+height = int(y / 5)
+width = int(x / 13)
+
+while mainLoop:
+    screen.fill(green)
+    for card in hand:
+        index = hand.index(card)
+        img = pygame.transform.scale(card.image, (width, height))
+        screen.blit(img, (width * index, y - height))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            mainLoop = False
+    pygame.display.update()
+
+pygame.quit()
